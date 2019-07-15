5df65a5dcac5145c196ed2936cfa92099cd01c4c
Hackbright Student <no-reply@hackbrightacademy.com>
2019-07-15 15:49:47 -0700

    added option for user to add restaurant

--- ratings.py
+++ ratings.py
@@ -2,11 +2,10 @@
 
 
 # put your code here
-def print_resturant_ratings(txtfile):
+def print_restaurant_ratings(txtfile):
     """prints all resturants by alphabetical order and their associated rating
     """
-    #restaurant_file = open(txtfile)
-
+   
     with open(txtfile,'r') as f:
         restaurant_file = f.read()
 
@@ -19,10 +18,28 @@ def print_resturant_ratings(txtfile):
 
         restaurant_dict[rest_name] = rest_rating
 
+    # ask user if they want to add a restaurant, if yes add it to the dictionary
+    while True:
+        add_rest_choice = input("Would you like to add a restuarant? Y or N: ").upper()
 
-    sorted_resturants = sorted(restaurant_dict.items())
+        if add_rest_choice.startswith("Y"):
+            add_restaurant_name = input("Add a new restaurant: ")
+            add_restaurant_rating = input(f"Add {add_restaurant_name}'s rating: ")
 
-    for restaurant in sorted_resturants:
+            restaurant_dict[add_restaurant_name] = add_restaurant_rating
+            print(f"Thank you. {add_restaurant_name}: {add_restaurant_rating} has been added.")
+            print()
+        else:
+            print()
+            break
+
+    sorted_restaurants = sorted(restaurant_dict.items())
+
+    for restaurant in sorted_restaurants:
         print(f"{restaurant[0]} is rated at {restaurant[1]}.")
+
+    # return restaurant_dict
+
+
 
    
